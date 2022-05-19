996a0337983cb4213418905c88abcd5148a08c9d
sriya-g <75285747+sriya-g@users.noreply.github.com>
2022-05-18 21:01:25 -0400

    update
    fixed water issue

--- mapgen.py
+++ mapgen.py
@@ -4,31 +4,31 @@ import pandas as pd
 import seaborn as sns
 from matplotlib.colors import LinearSegmentedColormap
 
-mape = [[0]*10, [0]*10, [0]*10, [0]*10, [0]*10, [0]*10, [0]*10, [0]*10, [0]*10, [0]*10]
+mape = [[0] * 10, [0] * 10, [0] * 10, [0] * 10, [0] * 10, [0] * 10, [0] * 10, [0] * 10, [0] * 10, [0] * 10]
 print(mape)
-for i in range (0, 10):
-    for j in range (0, 10):
+for i in range(0, 10):
+    for j in range(0, 10):
         mape[i][j] = random.randrange(1, 101)
-for i in range (0, 10):
-    for j in range (0, 10):
+for i in range(0, 10):
+    for j in range(0, 10):
         if mape[i][j] < 11:
-            mape[i][j] = 5 #enemy
+            mape[i][j] = 0  # enemy
         elif 11 <= mape[i][j] < 20:
-            mape[i][j] = 15 #water
-        elif 20 <= mape[i][j] <40:
-            mape[i][j] = 30 #forest
-        elif 40 <= mape[i][j] <50:
-            mape[i][j] = 45 #path
-        elif 60 <= mape[i][j] <70:
-            mape[i][j] = 65 #wall
-        elif 70 <= mape[i][j] <75:
-            mape[i][j] = 72 #cave
-        elif 75 <= mape[i][j] <80:
-            mape[i][j] = 77 #dungeon
-        elif 80 <= mape[i][j] <95:
-            mape[i][j] = 87 #soil
-        elif 95 <= mape[i][j] <100:
-            mape[i][j] = 97 #town
+            mape[i][j] = 12  # water
+        elif 20 <= mape[i][j] < 40:
+            mape[i][j] = 24  # forest
+        elif 40 <= mape[i][j] < 50:
+            mape[i][j] = 36  # path
+        elif 50 <= mape[i][j] < 60:
+            mape[i][j] = 48  # wall
+        elif 60 <= mape[i][j] < 65:
+            mape[i][j] = 60  # cave
+        elif 65 <= mape[i][j] < 70:
+            mape[i][j] = 72  # dungeon
+        elif 70 <= mape[i][j] < 95:
+            mape[i][j] = 84  # soil
+        elif 95 <= mape[i][j] < 100:
+            mape[i][j] = 96  # town
 df = pd.DataFrame(mape)
 print(df)
 myColors = ('r', 'b', 'g', '#bd8d7b', '#697689', '#673ab7', '#9f0500', '#795548', 'c')
